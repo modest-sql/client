21934038562bad5202227cbfb4c82d9c457f8561
Jesus Murillo <chusmurillo9@gmail.com>
2017-10-24 20:05:21 -0600

    [Update] DB Metadata(testing) - Fixed listing of only a single table, now displays a db with various tables.

--- VisualSQL/VisualSQL/ClientForm.cs
+++ VisualSQL/VisualSQL/ClientForm.cs
@@ -16,7 +16,7 @@ namespace VisualSQL
     {
         private string ipAddress;
         private string portNumber;
-        private bool testing = false;
+        private bool testing = true;
         private TcpClient client;
         private NetworkStream nwStream;
 
@@ -53,7 +53,8 @@ namespace VisualSQL
                 if (testing)
                 {
                     json = @"[{""Name"":""AAA"",""Age"":""22"",""Job"":""PPP""},{""Name"":""BBB"",""Age"":""25"",""Job"":""QQQ""},{""Name"":""CCC"",""Age"":""38"",""Job"":""RRR""}]";
-                    db_data = @"{""Name"": ""Mocca DB"",""ColumnNames"": [""First Name"", ""Second Name"", ""First Last Name"", ""Second Last Name"", ""Age"", ""Married""],""ColumnTypes"": [""char[100]"", ""char[100]"", ""char[100]"", ""char[100]"", ""int"", ""boolean""]}";
+                    //db_data = @"{""Name"": ""Mocca DB"",""ColumnNames"": [""First Name"", ""Second Name"", ""First Last Name"", ""Second Last Name"", ""Age"", ""Married""],""ColumnTypes"": [""char[100]"", ""char[100]"", ""char[100]"", ""char[100]"", ""int"", ""boolean""]}";
+                    db_data = @"{""DB_Name"": ""Mocca DB"",""Tables"": [{""Table_Name"": ""Employee"", ""ColumnNames"": [""First Name"", ""Second Name"", ""First Last Name"", ""Second Last Name"", ""Age"", ""Married""],""ColumnTypes"": [""char[100]"", ""char[100]"", ""char[100]"", ""char[100]"", ""int"", ""boolean""]},{""Table_Name"": ""Department"", ""ColumnNames"": [""Department Name"", ""Description"", ""Address""],""ColumnTypes"": [""char[100]"", ""char[100]"", ""char[100]""]}]}";
                     sql_text.Text = ipAddress + ":" + portNumber;
 
                     DB_Metadata db_meta = new DB_Metadata();
@@ -92,17 +93,32 @@ namespace VisualSQL
                 listBox1.Items.Add(listitem);
             }*/
 
-            listBox1.Items.Add(dbMeta.Name);
+            /*listBox1.Items.Add(dbMeta.Name);
             for (int i = 0; i < dbMeta.ColumnNames.Length; i++)
             {
                 listBox1.Items.Add(dbMeta.ColumnNames[i] + "(" + dbMeta.ColumnTypes[i] + ")");
+            }*/
+
+            listBox1.Items.Add(dbMeta.DB_Name);
+            foreach (var table in dbMeta.Tables)
+            {
+                listBox1.Items.Add("\t" + table.Table_Name);
+                for (int i = 0; i < table.ColumnNames.Length; i++)
+                {
+                    listBox1.Items.Add("\t\t" + table.ColumnNames[i] + " (" + table.ColumnTypes[i] + ")");
+                }
             }
         }
 
         private void check_column_type_integrity(DB_Metadata dbMeta)
         {
-            if (dbMeta.ColumnNames.Length != dbMeta.ColumnTypes.Length)
-                throw new Exception("Hey! Column count and type count don't match!");
+            /*if (dbMeta.ColumnNames.Length != dbMeta.ColumnTypes.Length)
+                throw new Exception("Hey! Column count and type count don't match!");*/
+            foreach (var table in dbMeta.Tables)
+            {
+                if (table.ColumnNames.Length != table.ColumnTypes.Length)
+                    throw new Exception("Hey! Column count and type count don't match in table " + table.Table_Name);
+            }
         }
 
         private void close_connection()
@@ -138,7 +154,13 @@ namespace VisualSQL
 
     class DB_Metadata
     {
-        public string Name { get; set; }
+        public string DB_Name { get; set; }
+        public DB_Table[] Tables { get; set; }
+    }
+
+    class DB_Table
+    {
+        public string Table_Name { get; set; }
         public string[] ColumnNames { get; set; }
         public string[] ColumnTypes { get; set; }
     }
